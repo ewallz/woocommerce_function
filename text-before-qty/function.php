886d3322e64857e4dcf920f44777264848799f1d
ewallz <archonic08@gmail.com>
2018-11-18 22:12:54 +0800

    add different cat text

--- text-before-qty/function.php
+++ text-before-qty/function.php
@@ -1,6 +1,12 @@
+<?php
 //Add qty text before qty selector
-add_action( 'woocommerce_before_add_to_cart_quantity', 'ew_echo_qty_front_add_cart' );
- 
-function ew_echo_qty_front_add_cart() {
- echo '<div class="qty" style="padding:5px 10px 0 0;">Qty:</div>'; 
+add_action( 'woocommerce_before_add_to_cart_quantity', 'adv_echo_qty_front_add_cart', 100 );
+
+function adv_echo_qty_front_add_cart() {
+        if(is_product() && has_term( 'adventure', 'product_cat' )){
+            echo '<div class="qty" style="padding:5px 10px 0 0;">No. of Travelers:</div>'; 
+        }
+        else {
+			         echo '<div class="qty" style="padding:5px 10px 0 0;">Qty:</div>'; 
+		}
 }
