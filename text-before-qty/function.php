db2b3969459a2c0085a4eb4092559fd32630937b
ewallz <archonic08@gmail.com>
2018-11-18 23:59:36 +0800

    add pax no

--- text-before-qty/function.php
+++ text-before-qty/function.php
@@ -6,6 +6,9 @@ function adv_echo_qty_front_add_cart() {
         if(is_product() && has_term( 'adventure', 'product_cat' )){
             echo '<div class="qty" style="padding:5px 10px 0 0;">No. of Travelers:</div>'; 
         }
+	if(is_product() && has_term( 'outbound', 'product_cat' )){
+            echo '<div class="qty" style="padding:5px 10px 0 0;">No. of Pax:</div>'; 
+        }
         else {
 			         echo '<div class="qty" style="padding:5px 10px 0 0;">Qty:</div>'; 
 		}
